f4866005cc88a9d1113465c5ba11231885255dbc
ekaterina <whitedevil93@mail.ru>
2023-02-21 01:30:36 +0300

    натуральные числа от N до 1

---
+++ дз9.2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Показать натуральные числа от N до 1, N задано
+
+/*void PrintNumber(int n)
+{
+    if (n < 1) return;
+    Console.Write(n + " ");
+    PrintNumber(n - 1);
+}
+PrintNumber(5);
+*/
+
+
+Console.WriteLine();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+string NumbersRec(int a, int N)
+{
+  if (N <= a) return $"{a} " + NumbersRec(a - 1, N);
+  else return String.Empty;
+}
+Console.WriteLine(NumbersRec(N, 1));
+
