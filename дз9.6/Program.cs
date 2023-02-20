8d83c537228083973c472c69c44bd9d8e4817a69
ekaterina <whitedevil93@mail.ru>
2023-02-21 02:40:58 +0300

    функция аккермана

---
+++ дз9.6/Program.cs
@@ -0,0 +1,23 @@
+﻿// Написать программу вычисления функции Аккермана
+
+Console.Write("Введите первое число: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите второе число: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+int A(int n, int m)
+{
+    if (n == 0)
+    {
+        return m + 1;
+    }
+    if (n > 0 && m == 0)
+    {
+        return A(n - 1, 1);
+    }
+    if (n > 0 && m > 0)
+    {
+        return A(n - 1, A(n, m - 1));
+    }
+    return A(n, m);
+}
+Console.WriteLine($"Значение функции Аккермана = {A(n, m)}");
