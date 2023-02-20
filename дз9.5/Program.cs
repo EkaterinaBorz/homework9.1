93c5a30852f0b1d958ba5b21063fa5cfed219524
ekaterina <whitedevil93@mail.ru>
2023-02-21 02:12:53 +0300

    сумма цифр числа

---
+++ дз9.5/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти сумму цифр числа
+
+Console.WriteLine();
+int SumDigits(int number)
+{
+    if (number < 1) return 0;
+    return SumDigits(number / 10) + number % 10;
+}
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine() ??"0");
+Console.WriteLine($"Сумма цифр числа = {SumDigits(number)}");
