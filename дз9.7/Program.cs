39d2405ba37b7061fb912ed7bcc5afa3b1696bfe
ekaterina <whitedevil93@mail.ru>
2023-02-22 00:41:17 +0300

    число а в степени

---
+++ дз9.7/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу возведения числа А в целую стень B
+
+int Numbers(int numberA, int numberB)
+{
+    if (numberB < 1) return 1;
+    return numberA * (Numbers(numberA, numberB - 1));
+}
+
+Console.Write("Введите число: ");
+int numberA = int.Parse(Console.ReadLine()?? "0");
+Console.Write("Введите возводимую степень: ");
+int numberB = int.Parse(Console.ReadLine()?? "0");
+
+Console.WriteLine($"Число {numberA} в степени {numberB} = {Numbers(numberA, numberB)}");
